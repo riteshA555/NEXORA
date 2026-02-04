e69178a37a3f627f502be091289439514a839208
Ritesh <rites@example.com>
2026-02-04 06:43:30 +0530

    chore: explicitly define rollup input entry point

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
+import { resolve } from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+            },
+        },
+    },
     plugins: [
         react(),
         VitePWA({
